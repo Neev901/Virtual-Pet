808ad83ed776aa1e00b3cd6d7cf938ea880e627d
Neev901 <67997288+Neev901@users.noreply.github.com>
2020-11-11 19:45:44 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,59 @@
+//Create variables here
+var dog_sprite, dog, happyDog, DB, foodS, data_listner;
+
+const database = firebase.database();
+const foodStock = database.ref('Food/');
+
+function preload()
+{
+  //load images here
+  dog = loadImage("./Dog.png")
+  happyDog = loadImage("./happydog.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog_sprite = createSprite(250,250,50,50);
+  dog_sprite.addImage("normal_dog", dog);
+  dog_sprite.addImage("Happy_dog", happyDog);
+  dog_sprite.visible = true;
+  dog_sprite.scale = 0.3;
+  foodStock.on('value', readStock);
+  foodStock.child('Item_value').on('value', async (val)=>{
+    foodS = await val.val();
+  })
+}
+
+
+function draw() {  
+  background(46,139,87);
+  if(keyWentDown(UP_ARROW)){
+    if(foodS !== 0){
+    foodS = foodS - 1;
+    }
+    writeStock(foodS);
+    dog_sprite.changeImage("Happy_dog");
+  }
+
+  drawSprites();
+  //add styles here
+  fill("white"); 
+  textSize(20);
+  text("Click Up Arrow to feed Bailey milk", 10, 50);
+  text("Milk Bottles Left: " + foodS, 300, 450);
+  fill("Red");
+  textSize(40);
+  stroke(100); 
+  text("Bailey", 195, 395)
+}
+
+function writeStock(value){
+   foodStock.update({
+     Item_value : value
+   })
+}
+
+
+function readStock(data){
+  data_listner = data.val();
+}
